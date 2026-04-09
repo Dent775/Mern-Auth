53ac62ee177cf087f880082e056a00f2f3ea073a
Dent775 <dentsec151@gmail.com>
2026-04-09 17:12:30 +0530

    Fix import path for Appcontent in EmailVerify

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,7 +1,7 @@
 import React,{useContext, useEffect, useRef} from 'react'
 import { assets } from '../assets/assets'
 import axios from 'axios';
-import { Appcontent } from '../context/appContext';
+import { Appcontent } from '../context/AppContext';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
